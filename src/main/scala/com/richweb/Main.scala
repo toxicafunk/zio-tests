5484de181dd33d46f8a00675dc5d30357b45095b
Eric Rodriguez <eric.rodriguez@eniro.com>
2019-07-29 12:12:07 +0200

    List implementation

--- src/main/scala/com/richweb/Main.scala
+++ src/main/scala/com/richweb/Main.scala
@@ -19,38 +19,54 @@ import org.apache.kafka.common.serialization.StringSerializer
 
 object Main extends App {
 
-  val readFile: String => Stream[Nothing, String] = f =>
-  Stream.fromIterable(
-    Source
-      .fromInputStream(
-        this.getClass.getResourceAsStream(f)
-      )
-      .getLines()
-      .toIterable
-  )
+  val readFile: String => Task[List[String]] = f =>
+    Task.effect(
+      Source
+        .fromInputStream(
+          this.getClass.getResourceAsStream(f)
+        )
+        .getLines()
+        .toList
+    )
 
-  val toJson: String => Json = txt => parse(txt) match {
-    case Right(j) => j
-    case Left(failure) => Json.fromString(s"$failure")
-  }
+  val toJson: String => Json = txt =>
+    parse(txt) match {
+      case Right(j)      => j
+      case Left(failure) => Json.fromString(s"$failure")
+    }
 
   val idL = root.id.string
 
-  val producer = KafkaProducer(
-    Conf(new StringSerializer(), new StringSerializer(), bootstrapServers = "172.18.0.2:9092,172.18.0.4:9092,172.18.0.5:9092")
+  val producer = Task.effect(
+    KafkaProducer(
+      Conf(
+        new StringSerializer(),
+        new StringSerializer(),
+        bootstrapServers = "172.18.0.2:9092,172.18.0.4:9092,172.18.0.5:9092"
+      )
+    )
   )
 
-  val sendMessage: String => Task[RecordMetadata] = (data: String) => ZIO.fromFuture ( implicit ctx => {
-    val record = KafkaProducerRecord("julio.genio.stream", idL.getOption(toJson(data)).getOrElse(""), data)
-    producer.send(record)
-  })
+  val sendMessage
+      : (KafkaProducer[String, String], String) => Task[RecordMetadata] =
+    (producer, data) =>
+      ZIO.fromFuture(implicit ctx => {
+        val record = KafkaProducerRecord(
+          "julio.genio.stream",
+          idL.getOption(toJson(data)).getOrElse(""),
+          data
+        )
+        producer.send(record)
+      })
 
-  //@SuppressWarnings(Array("org.wartremover.warts.Any"))
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
   override def run(args: List[String]): ZIO[Environment, Nothing, Int] = {
-    readFile("/msgs100k.json")
-      .tap(l => sendMessage(l) >>= (rmd => putStrLn(rmd.toString())))
-      //.foreach(l => sendMessage(l) >>= (rmd => putStrLn(rmd.toString())))
-      .runDrain
-      .foldM(_ => UIO(1), _ => UIO(0))
+    val rmds = for {
+      prd <- producer
+      lst <- readFile("/msgs100k.json")
+      rmd <- Task.foreach(lst)(l => sendMessage(prd, l))
+      _   <- ZIO.foreach_(rmd)(md => putStrLn(md.toString()))
+    } yield ()
+    rmds.foldM(_ => UIO(1), _ => UIO(0))
   }
 }
